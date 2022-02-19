a061a85386d32160eacedbc0983b7732952141ff
Aidan Pavlik <apavlik@hawk.iit.edu>
2022-02-19 11:30:47 -0800

    Start creation of inventory

---
+++ main.js
@@ -0,0 +1,13 @@
+const anchorOne = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+const anchorTwo = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+const anchorThree = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+const anchorFour = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+const anchorFive = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+const anchorSix = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+const anchorSeven = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+const anchorEight = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+const anchorNine = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+const anchorTen = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+
+
+const inventory [anchorOne, anchorTwo, anchorThree, anchorFour, anchorFive, anchorSix, anchorSeven, anchorEight, anchorNine, anchorTen];
