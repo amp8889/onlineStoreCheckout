206fe70e98764c3ded8f1acd70822ac74e678b84
Aidan Pavlik <apavlik@hawk.iit.edu>
2022-02-23 14:09:15 -0800

    Add images in

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
 document.querySelector('html').className = 'js';
 
-const anchorOne = {name:"4 Pronged Glory", price:45.99, weight:87, image:"blue"};
-const anchorTwo = {name:"Built for a Battleship", price:1699.99, weight:762, image:"blue"};
-const anchorThree = {name:"Black Pearl", price:75.99, weight:45, image:"blue"};
-const anchorFour = {name:"Oceans Gem", price:450.99, weight:120, image:"blue"};
-const anchorFive = {name:"Modern", price:49.99, weight:35, image:"blue"};
-const anchorSix = {name:"Pirates Treasure", price:12000.99, weight:12, image:"blue"};
-const anchorSeven = {name:"Rusted Beauty", price:9.99, weight:63, image:"blue"};
-const anchorEight = {name:"The Grappler", price:29.99, weight:55, image:"blue"};
-const anchorNine = {name:"The Spoon", price:43.99, weight:88, image:"blue"};
-const anchorTen = {name:"Wooden Master", price:7.99, weight:5, image:"blue"};
+const anchorOne = {name:"4 Pronged Glory", price:45.99, weight:87, image:"assets/4Prong.jpg"};
+const anchorTwo = {name:"Built for a Battleship", price:1699.99, weight:762, image:"assets/battleShip.jpg"};
+const anchorThree = {name:"Black Pearl", price:75.99, weight:45, image:"assets/blackPearl.jpg"};
+const anchorFour = {name:"Oceans Gem", price:450.99, weight:120, image:"assets/blueOcean.jpg"};
+const anchorFive = {name:"Modern", price:49.99, weight:35, image:"assets/modern.jpg"};
+const anchorSix = {name:"Pirates Treasure", price:12000.99, weight:12, image:"assets/PiratesBounty.jpeg"};
+const anchorSeven = {name:"Rusted Beauty", price:9.99, weight:63, image:"assets/rustedBeauty.jpg"};
+const anchorEight = {name:"The Grappler", price:29.99, weight:55, image:"assets/theGrapplingHook.jpg"};
+const anchorNine = {name:"The Spoon", price:43.99, weight:88, image:"assets/theSpoon.jpg"};
+const anchorTen = {name:"Wooden Master", price:7.99, weight:5, image:"assets/woodenMaster.jpg"};
 
 
 const inventory = [anchorOne, anchorTwo, anchorThree, anchorFour, anchorFive, anchorSix, anchorSeven, anchorEight, anchorNine, anchorTen];
@@ -24,24 +24,31 @@ generateCartBT.addEventListener('click', function(){
   document.getElementById('nameOne').innerText = "Name: " + inventory[num].name;
   document.getElementById('priceOne').innerText = "Price: " + inventory[num].price;
   document.getElementById('weightOne').innerText = "Weight: " + inventory[num].weight;
+  document.getElementById('imageOne').src = inventory[num].image;
 
   var num = Math.floor(Math.random() * (10 - 0) + 0);
 
   document.getElementById('nameTwo').innerText = "Name: " + inventory[num].name;
   document.getElementById('priceTwo').innerText = "Price: " + inventory[num].price;
   document.getElementById('weightTwo').innerText = "Weight: " + inventory[num].weight;
+  document.getElementById('imageTwo').src = inventory[num].image;
+
 
   var num = Math.floor(Math.random() * (10 - 0) + 0);
 
   document.getElementById('nameThree').innerText = "Name: " + inventory[num].name;
   document.getElementById('priceThree').innerText = "Price: " + inventory[num].price;
   document.getElementById('weightThree').innerText = "Weight: " + inventory[num].weight;
+  document.getElementById('imageThree').src = inventory[num].image;
+
 
   var num = Math.floor(Math.random() * (10 - 0) + 0);
 
   document.getElementById('nameFour').innerText = "Name: " + inventory[num].name;
   document.getElementById('priceFour').innerText = "Price: " + inventory[num].price;
   document.getElementById('weightFour').innerText = "Weight: " + inventory[num].weight;
+  document.getElementById('imageFour').src = inventory[num].image;
+
 
 
   console.log('TeSTING')
