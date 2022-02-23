9969d587f0c0f23ecb8a5791e92e56a2c1660b82
Aidan Pavlik <apavlik@hawk.iit.edu>
2022-02-23 13:48:51 -0800

    Create random cart generator

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
 document.querySelector('html').className = 'js';
 
-const anchorOne = {name:"Blue Devil", price:45.99, weight:50, image:"blue"};
-const anchorTwo = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
-const anchorThree = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
-const anchorFour = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
-const anchorFive = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
-const anchorSix = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
-const anchorSeven = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
-const anchorEight = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
-const anchorNine = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
-const anchorTen = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+const anchorOne = {name:"Blue Devil1", price:45.99, weight:50, image:"blue"};
+const anchorTwo = {name:"Blue Devil2", price:"Doe", weight:50, image:"blue"};
+const anchorThree = {name:"Blue Devil3", price:"Doe", weight:50, image:"blue"};
+const anchorFour = {name:"Blue Devil4", price:"Doe", weight:50, image:"blue"};
+const anchorFive = {name:"Blue Devil5", price:"Doe", weight:50, image:"blue"};
+const anchorSix = {name:"Blue Devil6", price:"Doe", weight:50, image:"blue"};
+const anchorSeven = {name:"Blue Dev7il", price:"Doe", weight:50, image:"blue"};
+const anchorEight = {name:"Blue Devil8", price:"Doe", weight:50, image:"blue"};
+const anchorNine = {name:"Blue Devil9", price:"Doe", weight:50, image:"blue"};
+const anchorTen = {name:"Blue Devil10", price:"Doe", weight:50, image:"blue"};
 
 
 const inventory = [anchorOne, anchorTwo, anchorThree, anchorFour, anchorFive, anchorSix, anchorSeven, anchorEight, anchorNine, anchorTen];
@@ -19,11 +19,33 @@ var generateCartBT = document.getElementById('GCBT');
 
 generateCartBT.addEventListener('click', function(){
 
-  document.getElementById('nameOne').innerText = "Name: " + anchorOne.name;
-  document.getElementById('priceOne').innerText = "Price: " + anchorOne.price;
-  document.getElementById('weightOne').innerText = "Weight: " + anchorOne.weight;
+  var num = Math.floor(Math.random() * (10 - 0) + 0);
+
+  document.getElementById('nameOne').innerText = "Name: " + inventory[num].name;
+  document.getElementById('priceOne').innerText = "Price: " + inventory[num].price;
+  document.getElementById('weightOne').innerText = "Weight: " + inventory[num].weight;
+
+  var num = Math.floor(Math.random() * (10 - 0) + 0);
+
+  document.getElementById('nameTwo').innerText = "Name: " + inventory[num].name;
+  document.getElementById('priceTwo').innerText = "Price: " + inventory[num].price;
+  document.getElementById('weightTwo').innerText = "Weight: " + inventory[num].weight;
+
+  var num = Math.floor(Math.random() * (10 - 0) + 0);
+
+  document.getElementById('nameThree').innerText = "Name: " + inventory[num].name;
+  document.getElementById('priceThree').innerText = "Price: " + inventory[num].price;
+  document.getElementById('weightThree').innerText = "Weight: " + inventory[num].weight;
+
+  var num = Math.floor(Math.random() * (10 - 0) + 0);
+
+  document.getElementById('nameFour').innerText = "Name: " + inventory[num].name;
+  document.getElementById('priceFour').innerText = "Price: " + inventory[num].price;
+  document.getElementById('weightFour').innerText = "Weight: " + inventory[num].weight;
+
 
   console.log('TeSTING')
+  console.log(num);
 
 
 });
