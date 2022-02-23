0550e71de89c88ee9dfad2772be973dd91d870d2
Aidan Pavlik <apavlik@hawk.iit.edu>
2022-02-23 13:37:16 -0800

    Start creating products for the cart

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const anchorOne = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
+
+document.querySelector('html').className = 'js';
+
+const anchorOne = {name:"Blue Devil", price:45.99, weight:50, image:"blue"};
 const anchorTwo = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
 const anchorThree = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
 const anchorFour = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
@@ -10,4 +13,19 @@ const anchorNine = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
 const anchorTen = {name:"Blue Devil", price:"Doe", weight:50, image:"blue"};
 
 
-const inventory [anchorOne, anchorTwo, anchorThree, anchorFour, anchorFive, anchorSix, anchorSeven, anchorEight, anchorNine, anchorTen];
+const inventory = [anchorOne, anchorTwo, anchorThree, anchorFour, anchorFive, anchorSix, anchorSeven, anchorEight, anchorNine, anchorTen];
+
+var generateCartBT = document.getElementById('GCBT');
+
+generateCartBT.addEventListener('click', function(){
+
+  document.getElementById('nameOne').innerText = "Name: " + anchorOne.name;
+  document.getElementById('priceOne').innerText = "Price: " + anchorOne.price;
+  document.getElementById('weightOne').innerText = "Weight: " + anchorOne.weight;
+
+  console.log('TeSTING')
+
+
+});
+
+console.log('TeSTING2')
